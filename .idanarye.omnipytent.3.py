eca017dc1038cb036bf905af6a89ca5c92e0e097
IdanArye <idanarye@gmail.com>
2020-12-04 18:30:14 +0200

    Add Omnipytent task `download_missing_input`

--- .idanarye.omnipytent.3.py
+++ .idanarye.omnipytent.3.py
@@ -2,6 +2,8 @@ import vim
 from omnipytent import *
 from omnipytent.ext.idan import *
 
+import re
+
 
 @task
 def compile(ctx):
@@ -17,3 +19,19 @@ def run(ctx):
 @task
 def download_input(ctx):
     cargo['aoc', 'input'] & BANG
+
+
+@task(alias=':0')
+def download_missing_input(ctx):
+
+    def gen(path, pattern):
+        pattern = re.compile(pattern)
+        for p in local.path(path):
+            if m:= pattern.search(p.basename):
+                day_text, = m.groups()
+                yield day_text
+
+    inputs = set(gen('input/2020', r'day(\d+).txt'))
+    days = set(gen('src', r'day(\d+).rs'))
+    for day in days - inputs:
+        cargo['aoc', 'input', '-d', day] & SH
