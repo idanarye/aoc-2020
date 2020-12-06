53e1f6dea9cb22b55f05f8fc07ba300c8a7de30c
Idan Arye <idanarye@weka.io>
2020-12-06 13:26:34 +0200

    Imrpove `day-template`

--- .idanarye.omnipytent.3.py
+++ .idanarye.omnipytent.3.py
@@ -21,7 +21,7 @@ def download_input(ctx):
     cargo['aoc', 'input'] & BANG
 
 
-@task(alias=':0')
+@task
 def download_missing_input(ctx):
 
     def gen(path, pattern):
@@ -35,3 +35,32 @@ def download_missing_input(ctx):
     days = set(gen('src', r'day(\d+).rs'))
     for day in days - inputs:
         cargo['aoc', 'input', '-d', day] & SH
+
+
+@task(alias=':0')
+def add_day(ctx, day_nr=99):
+    import copier
+    copier.copy(
+        '.copier/day-template',
+        '.',
+        dict(day=day_nr))
+
+    lib_main_file = local.path('src/lib.rs')
+
+    def gen_new_lines():
+        day_mod_pattern = re.compile(r'^pub mod day\d+;$');
+        it = iter(lib_main_file.read('utf8').splitlines())
+
+        for line in it:
+            if not day_mod_pattern.match(line):
+                break
+            yield line
+        else:
+            assert False, 'found no end?'
+
+        yield f'pub mod day{day_nr}'
+        yield line
+
+        yield from it
+
+    lib_main_file.write('\n'.join(gen_new_lines()))
